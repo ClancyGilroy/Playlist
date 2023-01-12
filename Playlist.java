ccbcddb178d765a235f525e28dda38e6b80922ea
Clancy Gilroy <ashrsingla@gmail.com>
2023-01-12 10:17:55 -0800

    finshed all playlist other than removeunliked

--- Playlist.java
+++ Playlist.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.io.*;
 
 /**
  * The Playlist class, which will keep track of a playlist of Song objects
@@ -41,7 +42,49 @@ public class Playlist {
             String currentname = currentsong.getname();
             String currentartist = currentsong.getartist();
             int currentlength = currentsong.getlength();
-            //this method is unfinished
+            String currentformattedlength = "(" + currentlength / 60 + ":" + currentlength % 60 + ")";
+            String y = "\"" + currentname + "\" by " + currentartist + currentformattedlength;
+            x.add(y);
         }
+        return x;
+      }
+      public ArrayList<String> examineliked(){
+        ArrayList<String> x = new ArrayList<>();
+        for(int i = 0; i < playlist.size(); i ++){
+            Song currentsong = playlist.get(i);
+            String currentname = currentsong.getname();
+            String currentartist = currentsong.getartist();
+            int currentlength = currentsong.getlength();
+            boolean currentliked = currentsong.getliked();
+            String currentformattedlength = "(" + currentlength / 60 + ":" + currentlength % 60 + ")";
+            String y = "\"" + currentname + "\" by " + currentartist + currentformattedlength;
+            if(currentliked){
+                x.add(y);
+            }
+        }
+        return x;
+      }
+      public String duration(){
+        int sum = 0;
+        for(int i = 0; i < playlist.size(); i++){
+            Song currentsong = playlist.get(i);
+            int currentlength = currentsong.getlength();
+            sum += currentlength;
+        }
+        String x = sum / 60 + ":" + sum % 60;
+        return x;
+      }
+      public void removeunliked(){
+        ArrayList<Integer> indexlist = new ArrayList<>();
+        for(int i = 0; i < playlist.size(); i++){
+            Song currentsong = playlist.get(i);
+            boolean currentliked = currentsong.getliked();
+            if(!currentliked){
+                indexlist.add(i);
+            }
+        }
+        Collections.sort(indexlist);
+        Collections.reverse(indexlist);  
+        //this method is not done 
       }
 }
