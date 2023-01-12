2229df1bb1ae3d3ef57f95308b1fbd0e826a637a
Clancy Gilroy <ashrsingla@gmail.com>
2023-01-12 09:04:53 -0800

    Coded Song.java class file

--- Song.java
+++ Song.java
@@ -5,18 +5,21 @@
  */
 public class Song {
     //Fields-- what information do we want each Song to store?
-
-
-
+    private boolean liked;
+    private int length;
+    private String name;
+    private String artist;
 
     /**
      * Constructor-- what information needs to be given to make a Song?
      * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
      */
-
-
-
-
+    public Song(int mylength, String myname, String myartist){
+        liked = false;
+        length = mylength;
+        name = myname;
+        artist = myartist;
+    }
 
      /**
       * Methods-- what will you want each Song to do?
@@ -25,6 +28,22 @@ public class Song {
       * song easily as well!
       * What kind of mutator (setter) methods will you need?
       */
-
-
+      public void like(){
+        liked = true;
+      }
+      public void unlike(){
+        liked = false;
+      }
+      public String getname(){
+        return name;
+      }
+      public String getartist(){
+        return artist;
+      }
+      public int getlength(){
+        return length;
+      }
+      public boolean getliked(){
+        return liked;
+      }
 }
